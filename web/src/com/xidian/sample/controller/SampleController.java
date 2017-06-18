9154c12e5bfe0e0e7eb3baa6d708fe59e9541ebd
xidianheyanjun <1538302885@qq.com>
2017-06-18 23:45:40 +0800

    输出图片流示例代码

--- web/src/com/xidian/sample/controller/SampleController.java
+++ web/src/com/xidian/sample/controller/SampleController.java
@@ -1,7 +1,16 @@
 package com.xidian.sample.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +36,21 @@ public class SampleController {
 		logger.info("name:" + name);
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("name", name);
-//		sampleService.executeSample(map);
+		// sampleService.executeSample(map);
 		return map;
+	}
+
+	@RequestMapping(value = "/image", method = { RequestMethod.POST,
+			RequestMethod.GET })
+	public void image(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		String path = request.getSession().getServletContext()
+				.getRealPath("image/hawk.png");
+		logger.info("image..." + path.toString());
+		InputStream is = new FileInputStream(path);
+		BufferedImage bi = ImageIO.read(is);
+		response.setContentType("image/png");
+		OutputStream os = response.getOutputStream();
+		ImageIO.write(bi, "png", os);
 	}
 }
