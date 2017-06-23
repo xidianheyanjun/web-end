f6883f33bf80a861aa70f8ee07f421d8d8d6d9ac
xidianheyanjun <1538302885@qq.com>
2017-06-24 01:05:02 +0800

    短信jar包

--- end/src/main/java/com/xidian/module/gb/GbController.java
+++ end/src/main/java/com/xidian/module/gb/GbController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.qcloud.sms.SmsSingleSender;
+import com.qcloud.sms.SmsSingleSenderResult;
+
 @Controller
 public class GbController {
 	private Logger logger = Logger.getLogger(getClass());
@@ -23,6 +26,15 @@ public class GbController {
 	@ResponseBody
 	public Object sample(String name) {
 		logger.info("name:" + name);
+		int appid = 123456;
+		String appkey = "1234567890abcdef1234567890abcdef";
+		try {
+			SmsSingleSender singleSender = new SmsSingleSender(appid, appkey);
+			SmsSingleSenderResult singleSenderResult = singleSender.send(0,
+					"86", "12345678902", "测试短信，普通单发，深圳，小明，上学。", "", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		Map<String, Object> map = new HashMap<String, Object>();
 		return map;
 	}
